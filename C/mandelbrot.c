b0f10a11379f29f7b5101214796ec9db74468ca8
Toxe <toxe@toxe.net>
2016-10-14 21:12:17 +0200

    Header Update

--- C/mandelbrot.c
+++ C/mandelbrot.c
@@ -1,13 +1,15 @@
 /*
  * C Mandelbrot.
  *
- * Usage: ./mandelbrot <image_width> <image_height> <iterations> <repetitions (1+)> <center x> <center y> <section height> <gradient filename> <output filename>
- * Example: ./mandelbrot 320 200 20 1 -0.5 0.0 2.0 blue.gradient mandelbrot.raw
+ * Usage: mandelbrot <image_width> <image_height> <iterations> <repetitions (1+)> <center x> <center y> <section height> <gradient filename> <output filename>
+ * Example: mandelbrot 320 200 20 1 -0.5 0.0 2.0 blue.gradient mandelbrot.raw
  *
  * Gradient file example:
  *   0.0: 0.0, 0.0, 0.0
  *   0.5: 0.0, 0.0, 1.0
  *   1.0: 1.0, 1.0, 1.0
+ *
+ * Tobias Brückner, 2016
  */
 
 #include <stdlib.h>
